6122e9589ece412b6ca357a046f1dc7fb84dcf9a
Nayab-kiran <nayabmughal586@gmail.com>
2026-05-06 07:14:21 +0500

    Your commit message here

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const protectedRoute = createRouteMatcher(['/dashboard(.*)']);
+const publicRoute = createRouteMatcher(['/','/features','/subjects','/pricing','contact']);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+
+  // If the route is protected and the user is not signed in, redirect them to sign-in
+  if (protectedRoute(req) && !userId) {
+    return redirectToSignIn();
+  }
+
+  // Public routes (like `/meeting`) don't require authentication
+  return;
+});
+
+export const config = {
+  matcher: [
+    '/((?!.+\\.[\\w]+$|_next).*)',
+    '/',
+    '/(api|trpc)(.*)',
+  ],
+};
